f82402cab7fa8fa1e41251e51bea2367163af163
PedroSehn <pedrorsehn@hotmail.com>
2023-02-02 13:10:02 -0300

    fix: aparentement resolve

--- inventory_report/reports/ReportTools/stringfy_items.py
+++ inventory_report/reports/ReportTools/stringfy_items.py
@@ -1,49 +1,10 @@
-mock = [
-    {
-        "id": 1,
-        "nome_do_produto": "MESA",
-        "nome_da_empresa": "Forces of Nature",
-        "data_de_fabricacao": "2022-05-04",
-        "data_de_validade": "2023-02-09",
-        "numero_de_serie": "FR48",
-        "instrucoes_de_armazenamento": "Conservar ao abrigo de luz",
-    },
-    {
-        "id": 2,
-        "nome_do_produto": "MESA",
-        "nome_da_empresa": "Forces of Nature",
-        "data_de_fabricacao": "2022-05-04",
-        "data_de_validade": "2023-02-09",
-        "numero_de_serie": "FR48",
-        "instrucoes_de_armazenamento": "Conservar ao abrigo de luz",
-    },
-    {
-        "id": 3,
-        "nome_do_produto": "MESA",
-        "nome_da_empresa": "Zoios",
-        "data_de_fabricacao": "2022-05-04",
-        "data_de_validade": "2023-02-09",
-        "numero_de_serie": "FR48",
-        "instrucoes_de_armazenamento": "Conservar ao abrigo de luz",
-    },
-    {
-        "id": 1,
-        "nome_do_produto": "MESA",
-        "nome_da_empresa": "Pedro's",
-        "data_de_fabricacao": "2022-05-04",
-        "data_de_validade": "2023-02-09",
-        "numero_de_serie": "FR48",
-        "instrucoes_de_armazenamento": "Conservar ao abrigo de luz",
-    },
-]
 
 
 def stringfy_items(dict: dict):
-    companies_dict = {"Forces of Nature": 2, "Zoios": 1, "Pedro's": 1}
-    keys = companies_dict.keys()
+    keys = dict.keys()
     string_list = []
 
     for key in keys:
-        value = companies_dict[f"{key}"]
+        value = dict[f"{key}"]
         string_list.append(f"- {key}: {value}\n")
     return "".join(string_list)
